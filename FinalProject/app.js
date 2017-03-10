5721320a00e31f41a8ac24f7388a9d88e26e86e3
Arthur Ka Wai CHEUNG <20cheunga4@kgv.hk>
2017-03-10 18:21:14 +0800

    init commit

--- FinalProject/app.js
+++ FinalProject/app.js
@@ -30,12 +30,15 @@ function create(){
 	ledge.body.immovable = true;
 	//Add a player to my canvas
 	player = game.add.sprite(32, game.world.height-150,'dude')
-	game.physics.aracade.enable(player);
+	game.physics.arcade.enable(player);
 	player.body.bounce.y= 0.2;
 	player.body.gravity.y = 300;
+	player.body.collideWorldBounds = true;
 	//Defining our animations
 	player.animations.add('left',[0,1,2,3],10,true);
 	player.animations.add('right',[5,6,7,8],10,true);
+
+
 
 
 
